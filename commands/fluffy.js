377344e46ca5ffd3d745c3c0f6d6ead57a0ec4e4
Gabriel Pires e Peixotto <88248683+GPiresP@users.noreply.github.com>
2023-01-19 15:05:21 -0300

    Update fluffy message
     Added formated unix time message,

--- commands/fluffy.js
+++ commands/fluffy.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require("discord.js");
-const mensagem = `O Fluffy the Cup , nosso torneio de Guilty Gear - STRIVE -  para iniciantes, mais recente foi/será em 20/01 às 20:00 (Check in 19:00). 
+const mensagem = `O Fluffy the Cup , nosso torneio de Guilty Gear - STRIVE -  para iniciantes, mais recente foi/será em <t:1674255600:F> (Check in <t:1674252000:t>). 
 Para se inscrever mande seu perfil do challonge e rating update para algum organizador do torneio para se inscrever. Você receberá um convite no site para entrar no torneio. As regras estão na página do torneio: https://challonge.com/pt_BR/bm21o4y
 `;
 
