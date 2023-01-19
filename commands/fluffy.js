700a9a4ef40036e3eedd0ec509f1a48b0a6daf27
Madson369 <madsonlz369@gmail.com>
2023-01-19 14:43:49 -0300

    [FEAT]/nova mensagem da fluffy

--- commands/fluffy.js
+++ commands/fluffy.js
@@ -1,4 +1,7 @@
 const { SlashCommandBuilder } = require("discord.js");
+const mensagem = `O Fluffy the Cup , nosso torneio de Guilty Gear - STRIVE -  para iniciantes, mais recente foi/será em 20/01 às 20:00 (Check in 19:00). 
+Para se inscrever mande seu perfil do challonge e rating update para algum organizador do torneio para se inscrever. Você receberá um convite no site para entrar no torneio. As regras estão na página do torneio: https://challonge.com/pt_BR/bm21o4y
+`;
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -6,7 +9,7 @@ module.exports = {
     .setDescription("Detalhes sobre torneio!"),
   async execute(interaction) {
     await interaction.reply({
-      content: "Fluffy confia!",
+      content: mensagem,
       ephemeral: false,
     });
   },
