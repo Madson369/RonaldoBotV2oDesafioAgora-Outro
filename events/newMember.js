dd8b52851fda56a290e079934a78581d872903ef
Madson369 <madsonlz369@gmail.com>
2023-01-19 13:58:46 -0300

    feat/nova mensagem de welcome

--- events/newMember.js
+++ events/newMember.js
@@ -1,16 +1,20 @@
 const { Events } = require("discord.js");
 
 const channelId = "701571020942868581";
-const targetChannel = "701571021198590055";
+const rulesChannel = "701571021198590054";
+const rolesChannel = "701571021198590055";
 
 module.exports = {
   name: Events.GuildMemberAdd,
   once: false,
   execute(info) {
-    console.log("info", info.user);
     const message = `Teste teste teste <@${
       info.user.id
-    }> se liga no ${info.guild.channels.cache.get(targetChannel).toString()}`;
+    }> se liga nas regras ${info.guild.channels.cache
+      .get(rulesChannel)
+      .toString()} e pega os cargo la no
+    ${info.guild.channels.cache.get(rolesChannel).toString()}
+    `;
 
     const channel = info.guild.channels.cache.get(channelId);
     channel.send(message);
