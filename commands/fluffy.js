592f2b12a4fc266c144f135d422827eb506a223c
Madson369 <madsonlz369@gmail.com>
2023-01-19 14:30:56 -0300

    fix/corrigindo typo

--- commands/fluffy.js
+++ commands/fluffy.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder } = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("torneio")
+    .setName("fluffy")
     .setDescription("Detalhes sobre torneio!"),
   async execute(interaction) {
     await interaction.reply({
