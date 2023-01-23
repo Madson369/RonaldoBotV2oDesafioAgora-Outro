54462b6fafc68d7f2451a72d5bbd144fca15307f
Fl0wey57 <110173289+Fl0wey57@users.noreply.github.com>
2023-01-23 13:58:55 -0300

    feat/ criado comando de labbar

---
+++ commands/labbar.js
@@ -0,0 +1,26 @@
+const { SlashCommandBuilder } = require("discord.js");
+const { EmbedBuilder } = require("discord.js");
+
+
+//Creates a message embed with a Robo-Ky give, humbbly asking to lab
+const labMazeEmbed = new EmbedBuilder()
+    .setColor(0x14D514)
+    .setAuthor(
+        {name: "Maze"}
+        )
+    .setImage("https://media.tenor.com/pKwRy_WMQXAAAAAd/robo-ky-robo-ky-gym.gif")
+    .setTitle("VAI LABBAR!")
+    .setDescription("Faz bem para a saúde.")
+    .setFooter(
+        {text: "Provided by GGSTBR"}
+        );
+
+module.exports = {
+  disabled: false,
+  data: new SlashCommandBuilder()
+    .setName("labbar")
+    .setDescription("O Maze versão bot pede para tu labbar."),
+  async execute(interaction) {
+    await interaction.reply({ content: [labMazeEmbed], ephemeral: false });
+  },
+};
