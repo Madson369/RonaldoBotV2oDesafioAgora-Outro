63caa96d0d903e41529cb7ee4440da54b3145e6a
Madson369 <madsonlz369@gmail.com>
2023-02-08 13:02:10 -0300

    comando salario

---
+++ commands/salario.js
@@ -0,0 +1,17 @@
+/*
+Imprime os detalhes do Torneio 'Fluffy the Cup' mais recent
+*/
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  disabled: false,
+  data: new SlashCommandBuilder()
+    .setName("fluffy")
+    .setDescription("Detalhes sobre torneio!"),
+  async execute(interaction) {
+    await interaction.reply({
+      content: `me paga ae krl <:EpicSlayerMoment:1058482577826119800>`,
+      ephemeral: false,
+    });
+  },
+};
