c5ae220adc3462bbffd1d24aa40f6acee410539e
Madson369 <madsonlz369@gmail.com>
2023-04-20 16:55:27 -0300

    comando besta de mandar mensagem e habilitando o on message

---
+++ commands/say.js
@@ -0,0 +1,32 @@
+/*
+Tenta obter a frame data do golpe de um personagem a partir do dustloop.com
+*/
+
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  disabled: false,
+  data: new SlashCommandBuilder()
+    .setName("say")
+    .setDescription("!")
+    .addStringOption((option) =>
+      option.setName("text").setDescription("lança a voz").setRequired(true)
+    ),
+  async execute(interaction) {
+    console.log(interaction.user.id);
+    if (interaction.user.id === "158000799673221121") {
+      const text = interaction.options.getString("text");
+
+      await interaction.reply({
+        content: text,
+        ephemeral: false,
+      });
+      return;
+    }
+
+    await interaction.reply({
+      content: "you are not the chosen one",
+      ephemeral: false,
+    });
+  },
+};
--- events/onMessage.js
+++ events/onMessage.js
@@ -3,7 +3,7 @@ const { Events } = require("discord.js");
 module.exports = {
   name: Events.MessageCreate,
   async execute(mensagem) {
-    if (mensagem.author.id === "158000799673221121") {
+    {
       const reactionEmoji = mensagem.guild.emojis.cache.find(
         (emoji) => emoji.name === "fluffy"
       );
@@ -13,8 +13,34 @@ module.exports = {
       //     mensagem.react(reactionEmoji).then().catch(console.error);
       //     return;
       //   }
+      if (mensagem.content.toLowerCase() === "chat") {
+        const number = Math.random();
+        if (number <= 0.1) {
+          const opcoes = [
+            "que foi",
+            `"chat isso" "chat aquilo" how about you chat with some bitches huh? que tal em?            `,
+            "capivara",
+            "porfavor para o downplay",
+            "pfvr pede pra dia me pagar to passando fome",
+          ];
+          const opcaoSelecionada =
+            opcoes[Math.floor(Math.random() * opcoes.length)];
+
+          mensagem.reply(opcaoSelecionada).then(/* ... */).catch(console.error);
+          //   mensagem.react(reactionEmoji).then().catch(console.error);
+          return;
+        }
+      }
+      if (mensagem.content.toLowerCase() === "sabe") {
+        const number = Math.random();
+        if (number <= 0.1) {
+          mensagem.reply("sei").then(/* ... */).catch(console.error);
+          //   mensagem.react(reactionEmoji).then().catch(console.error);
+          return;
+        }
+      }
       if (mensagem.content.includes("ky")) {
-        if (Math.random() <= 0.01) {
+        if (Math.random() <= 0.04) {
           mensagem
             .reply("ky é muito based vdd")
             .then(/* ... */)
